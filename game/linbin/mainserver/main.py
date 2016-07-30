c07f61cbf4294f4e0ff36a175ed27a6d5cc7f0ef
alex.lin <linbin1253007885@gmail.com>
2016-07-31 00:22:42 +0800

    添加http异步

--- game/linbin/mainserver/main.py
+++ game/linbin/mainserver/main.py
@@ -5,7 +5,9 @@ import tornado.options
 import tornado.web
 import tornado.httpclient
 import tornado.gen
+from tornado.httpclient import AsyncHTTPClient
 from tornado.concurrent import run_on_executor
+import tornado.escape
 
 from mainserver.base import BaseHandle
 from mainserver.db import conn
@@ -47,11 +49,20 @@ class LoginHandler(BaseHandle):
         )
 
 
+class RequireHandle(BaseHandle):
+
+    @tornado.gen.coroutine
+    def get(self):
+        http_client = AsyncHTTPClient()
+        response = yield http_client.fetch("http://news-at.zhihu.com/api/4/news/latest")
+        r_data = tornado.escape.json_decode(response.body)
+        self.write(r_data)
+
+
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     app = tornado.web.Application(handlers=[
-            (r"/register", RegisterHandler), (r"/login", LoginHandler)])
+        (r"/register", RegisterHandler), (r"/login", LoginHandler), (r"/require", RequireHandle)])
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
-
